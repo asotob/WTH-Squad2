6db90c7901ca5b1bd34d90e0259849463fa01902
jmmoralesc26 <152323589+jmmoralesc26@users.noreply.github.com>
2023-11-30 13:12:32 -0600

    Update Startup.cs

--- Application/src/RazorPagesTestSample/Startup.cs
+++ Application/src/RazorPagesTestSample/Startup.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using RazorPagesTestSample.Data;
-
+// test
 namespace RazorPagesTestSample
 {
     public class Startup
