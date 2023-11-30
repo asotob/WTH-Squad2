ed5a5e74b3742346ee3f6bba538e5a7a74103c40
jmmoralesc26 <152323589+jmmoralesc26@users.noreply.github.com>
2023-11-30 13:19:21 -0600

    Update AppDbContext.cs

--- Application/src/RazorPagesTestSample/Data/AppDbContext.cs
+++ Application/src/RazorPagesTestSample/Data/AppDbContext.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-
+ //test
 namespace RazorPagesTestSample.Data
 {
     public class AppDbContext : DbContext
